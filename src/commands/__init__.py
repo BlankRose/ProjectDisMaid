51843088690bae24aa98b0bd5294c28624e8e931
Rosie <74193050+BlankRose@users.noreply.github.com>
2022-10-05 08:10:06 +0200

    Sighly changed commands init

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -1,13 +1,12 @@
 __all__ = ["hello", "random"]
 
+from src.commands import help
 import importlib
 import sys
 
 entries = {}
+__all__.append("help")
 for i in __all__:
 	name = "src.commands." + i
 	importlib.import_module(name)
 	entries[i] = getattr(sys.modules[name], i.capitalize())
-
-from src.commands import help
-entries["help"] = getattr(sys.modules["src.commands." + "help"], "Help")
